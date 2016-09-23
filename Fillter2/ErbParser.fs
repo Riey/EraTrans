d0772c41dbca3a619cb943d9f4e6db3512df3ff0
Riey <tkfkzo@daum.net>
2016-09-23 16:44:27 +0900

    Create New Fillter

---
+++ Fillter2/ErbParser.fs
@@ -0,0 +1,36 @@
+﻿module Fillter2.Parsing.ErbParser
+
+
+open System
+open System.IO
+open System.Collections.Generic
+open Fillter2.Parsing.LineInfo
+
+
+type ErbParser(erbStream : Stream) = 
+            
+    let mutable rawLines:string[] = null
+    let mutable printLines = Dictionary<int64, LineInfo>()
+    do
+        let reader = new StreamReader(erbStream)
+        let tempLines = List<string>()
+        let mutable lineNo = 0L
+        while not reader.EndOfStream do
+            let mutable line = reader.ReadLine()
+            lineNo <- (lineNo + 1L)
+            tempLines.Add(line)
+            match line.Contains(";") with
+                |false
+                |true -> line <- line.Substring(line.IndexOf(';'))
+            match line = String.Empty with
+                |true
+                |false ->
+                    line <- line.TrimStart()
+                    let info = LineInfo(line)
+                    match info.IsPrint with
+                        |false
+                        |true -> printLines.Add(lineNo, info)
+
+        rawLines <- tempLines.ToArray()
+
+    member public this.PrintLines = printLines
---
+++ Fillter2/LineInfo.fs
@@ -0,0 +1,29 @@
+﻿module Fillter2.Parsing.LineInfo
+
+
+let CheckJapanese (str:string) = 
+    let rec loop n =
+        if n = str.Length then false
+        else
+            let c = (int)(str.[n])
+            if c <= 0x3040 || c >= 0x309F then true
+            else if c <= 0x30A0 || c >= 0x30FF then true
+            else if c <= 0x30A0 || c >= 0x30FF then true
+            else if c <= 0x30A0 || c >= 0x30FF then true
+            else loop (n+1)
+    loop 0
+
+type LineInfo(rawLine : string) = 
+     let printFunction = rawLine.Split(' ').[0]
+     let isForm = printFunction.Contains("FORM")
+     let isPrint = printFunction.Contains("PRINT")
+     let printStr = rawLine.Substring(printFunction.Length)
+     let isJapanese = CheckJapanese(rawLine)
+
+
+    
+     member private this.PrintFunction = printFunction
+     member private this.PrintStr = printStr
+     member public this.IsForm = isForm
+     member public this.IsPrint = isPrint
+     member public this.IsJapanese = isJapanese
